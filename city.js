8672610b79e93ecbfb986dea5b096e7a3c74379d
test <test@Jonathans-MacBook.local>
2017-12-17 14:41:40 -0800

    cities page added

---
+++ city.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+console.log("Sanity Check");
+
+
+
+
+$(function() {
+    $('body').vegas({
+
+        slides: [
+            //Half Dome
+            { src: 'https://i.imgur.com/RksE6i3.jpg' },
+            //Beach
+            { src: 'https://i.imgur.com/7RvMse0.jpg' },
+            //Mountains
+            { src: 'https://i.imgur.com/4qRdqwe.jpg' },
+            //Flower
+            { src: 'https://i.imgur.com/OdiAj3D.jpg' },
+            //Flower Field
+            { src: 'https://i.imgur.com/4cF020v.jpg' }
+        ]
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+});
--- script.js
+++ script.js
@@ -8,10 +8,15 @@ $(function() {
     $('body').vegas({
 
         slides: [
+            //Half Dome
             { src: 'https://i.imgur.com/pRWZR0X.jpg' },
+            //Beach
             { src: 'https://i.imgur.com/AcR0hqH.jpg' },
+            //Mountains
             { src: 'https://i.imgur.com/sjgT51e.jpg' },
+            //Flower
             { src: 'https://i.imgur.com/Hw1arTI.jpg' },
+            //Flower Field
             { src: 'https://i.imgur.com/CmVVsxL.jpg' }
         ]
     });
