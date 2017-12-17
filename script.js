25f52acbf072d7297c4cffa48a634b48fae505b9
test <test@Jonathans-MacBook.local>
2017-12-17 13:58:13 -0800

    different slider added

--- script.js
+++ script.js
@@ -6,14 +6,22 @@ console.log("Sanity Check");
 
 $(function() {
     $('body').vegas({
+    delay: 7000,
+    timer: false,
+    shuffle: true,
+    firstTransition: 'fade',
+    firstTransitionDuration: 5000,
+    transition: 'slideDown2',
+    transitionDuration: 2000,
         slides: [
             { src: 'https://i.imgur.com/pRWZR0X.jpg' },
             { src: 'https://i.imgur.com/AcR0hqH.jpg' },
+            { src: 'https://i.imgur.com/sjgT51e.jpg' },
+            { src: 'https://i.imgur.com/Hw1arTI.jpg' },
             { src: 'https://i.imgur.com/CmVVsxL.jpg' }
         ]
     });
 });
-
 
 
 
