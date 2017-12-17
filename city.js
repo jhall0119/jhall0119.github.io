453134b1e7b5ad3fa5465252970fd368761bc3bc
test <test@Jonathans-MacBook.local>
2017-12-17 14:56:36 -0800

    adding flash transition

--- city.js
+++ city.js
@@ -3,24 +3,20 @@ console.log("Sanity Check");
 
 
 
-
 $(function() {
     $('body').vegas({
 
-        slides: [
-            //Half Dome
+       slides: [
             { src: 'https://i.imgur.com/RksE6i3.jpg' },
-            //Beach
-            { src: 'https://i.imgur.com/7RvMse0.jpg' },
-            //Mountains
-            { src: 'https://i.imgur.com/4qRdqwe.jpg' },
-            //Flower
+           { src: 'https://i.imgur.com/7RvMse0.jpg' },
+           { src: 'https://i.imgur.com/4qRdqwe.jpg' },
             { src: 'https://i.imgur.com/OdiAj3D.jpg' },
-            //Flower Field
             { src: 'https://i.imgur.com/4cF020v.jpg' }
-        ]
+        ],
+        transition: 'flash2',
+
     });
-});
+ });
 
 
 
